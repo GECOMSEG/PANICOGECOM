bb352c5dccc2aa6b92255cd3adbe0bc78d730169
GECOMSEG <gecomseguranca@gmail.com>
2026-09-22 18:47:11 -0300

    Update header styling in central_monitoramento.py

--- central_monitoramento.py
+++ central_monitoramento.py
@@ -78,7 +78,7 @@ window.addEventListener('load', async () => {{
 
 # === TÍTULO ===
 st.markdown("""
-<h1 style='text-align:center;color:#ff0000;margin:0.3rem 0;'>🚨GECOM SEGURANÇA</h1>
+<h1 style='text-align:center;color:#ff0000;margin:0.1rem 0;'>🚨GECOM SEGURANÇA</h1>
 <p style='text-align:center;'>PROTEÇÃO MÁXIMA ✅</p>
 """, unsafe_allow_html=True)
 
