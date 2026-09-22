70db249d2cc1777af8e256e45d508f06fabf078b
GECOMSEG <gecomseguranca@gmail.com>
2026-09-22 18:02:10 -0300

    Update gecom_panico.py

--- gecom_panico.py
+++ gecom_panico.py
@@ -20,7 +20,7 @@ headers = {
 
 # === TELA CHEIA — SEM ELEMENTOS EXTRAS ===
 st.components.v1.html("""
-<meta name="theme-color" content="#ff0000">
+<meta name="theme-color" content="#000000">
 <meta name="apple-mobile-web-app-capable" content="yes">
 <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
 <style>
